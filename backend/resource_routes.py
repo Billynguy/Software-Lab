e5b8c7dc14d9dd3e00af5e895499be715c06e9ca
Daniel Xie <daniel.xie@utexas.edu>
2022-10-22 16:26:09 -0500

    Resource routes use HWSet class

--- backend/resource_routes.py
+++ backend/resource_routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, make_response, session
+from flask import Blueprint, make_response
+from hardwareSet import HWSet
+from db_manager import DBManager
 
 resource_bp = Blueprint('resource', __name__)
 """Blueprint for api requests related to resource information.
@@ -8,33 +10,33 @@ resource_bp = Blueprint('resource', __name__)
 """
 
 
-@resource_bp.get('/resource/<string:hwset>/resource-info')
-def __resource_get_resource_info_for(hwset: str):
+@resource_bp.get('/resource/<string:hwset_name>/resource-info')
+def __resource_get_resource_info_for(hwset_name: str):
     """Get the hardware set's name, capacity, and availability.
 
     We make all types of errors look identical to make it harder to reverse engineer.
     :return: json response with data containing hardware set's name, capacity, and availability or 404 error
     """
 
-    # resource = get_hwset_collection().get_hwset(hwset)
-    # if resource is None:
+    hwset = HWSet.load_hwset(hwset_name)
+    if hwset is None:
+        return make_response({
+            'status': {
+                'success': False,
+                'reason': 'Unable to get resource information.',
+            }
+        }, 404)
+
     return make_response({
         'status': {
-            'success': False,
-            'reason': 'Unable to get resource information.',
+            'success': True,
+        },
+        'data': {
+            'name': hwset_name,
+            'capacity': hwset.get_capacity(),
+            'availability': hwset.get_availability(),
         }
-    }, 404)
-
-    # return make_response({
-    #     'status': {
-    #         'success': True,
-    #     },
-    #     'data': {
-    #         'name': resource.get_name(),
-    #         'capacity': resource.get_capacity(),
-    #         'availability': resource.get_availability(),
-    #     }
-    # }, 200)
+    }, 200)
 
 
 @resource_bp.get('/resource/resource-info')
@@ -45,26 +47,19 @@ def __resource_get_resource_info():
     :return: json response with data containing resource info or 404 error if not
     """
 
-    # resources = []
-    # for resource in get_hwset_collection():
-    #     resources.append({
-    #         'name': resource.get_name(),
-    #         'capacity': resource.get_capacity(),
-    #         'availability': resource.get_availability(),
-    #     })
-
-    # return make_response({
-    #     'status': {
-    #         'success': True,
-    #     },
-    #     'data': {
-    #         'resources': resources
-    #     }
-    # }, 200)
+    resources = []
+    for hwset_doc in DBManager.get_instance().get_hwsets_collection().find():
+        resources.append({
+            'name': hwset_doc['name'],
+            'capacity': hwset_doc['capacity'],
+            'availability': hwset_doc['availability'],
+        })
 
     return make_response({
         'status': {
-            'success': False,
-            'reason': 'Unable to get resource information.',
+            'success': True,
+        },
+        'data': {
+            'resources': resources
         }
-    }, 404)
+    }, 200)
