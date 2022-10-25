546bd82220f04563b8abdb5ed03c82673cbe62cd
BobbyChiu <bychiu@utexas.edu>
2022-10-25 15:40:02 -0500

    Added Project Details to each individual Project Page

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const ProjectDetails = () => {
 
-    const { id } = useParams()
+    const { id } = useParams();
+    const { data: project, error, isPending} = useFetch('http://localhost:8000/projects/' + id);
+
 
     return ( 
         <div className="project-details">
-            <h2>Project Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {project && (
+                <article>
+                    <h2>{project.title}</h2>
+                    <div>
+                        <p>HW Set 1: {`${project.hwset1}`}</p>
+                        <p>HW Set 2: {`${project.hwset2}`}</p>
+                    </div>
+                </article>
+            )}
         </div>
      );
 }
