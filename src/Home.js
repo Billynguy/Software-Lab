51308d4fbb09e41745dadd8c8427a0a438bfc623
BobbyChiu <bychiu@utexas.edu>
2022-10-25 14:24:56 -0500

    Made custom hook, useFetch, to modularize and reuse components

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,9 @@
 import { useState, useEffect } from "react";
 import ProjectList from "./ProjectList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [projects, setProjects] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    // COMMMENTED as we will eventually make delete request to server
-    // const handleDelete = (id) => {
-    //     const newProjects = projects.filter(project => project.id !== id); // leaves orig array unchanged, and returns a new array based on orig array
-    //     setProjects(newProjects);
-    // }
-
-    // useEffect fires on every render
-    useEffect(() => {                           // can't make this async
-        fetch('http://localhost:8000/projects')
-            .then(res => {                      // 'then' fires a function once the fetch promise has resolved, ie once we have the data back. Reponse object res is NOT the data
-                if(!res.ok){
-                    throw Error('Could not fetch the data for that resource.') // Can connect to server but response is not normal
-                }
-                return res.json()               // passes the json into a javascript object, the 'return res.json()' returns a promise b/c res.json() is aync
-            })
-            .then(data => {                     // we get the data now   
-                setProjects(data);
-                setIsPending(false);
-                setError(null); // gets rid of error message in (successful) subsequent requests
-            })
-            .catch(err => { // Catches any network error
-                setIsPending(false);
-                setError(err.message);
-            })       
-        
-    }, []);                                     // fetch this data once when the component renders
+    const { data: projects, isPending, error} = useFetch('http://localhost:8000/projects');
 
     return (  
         <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect} from 'react';
+
+const useFetch = (url) => { // custom hooks in react need to start with word 'use'
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {                           // can't make this async
+        fetch(url)
+            .then(res => {                      // 'then' fires a function once the fetch promise has resolved, ie once we have the data back. Reponse object res is NOT the data
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource.') // Can connect to server but response is not normal
+                }
+                return res.json()               // passes the json into a javascript object, the 'return res.json()' returns a promise b/c res.json() is aync
+            })
+            .then(data => {                     // we get the data now   
+                setData(data);
+                setIsPending(false);
+                setError(null); // gets rid of error message in (successful) subsequent requests
+            })
+            .catch(err => { // Catches any network error
+                setIsPending(false);
+                setError(err.message);
+            })       
+        
+    }, [url]); // whenever the url changes, this function will be rerun to get the data
+
+    return { data, isPending, error}
+}
+
+export default useFetch;
