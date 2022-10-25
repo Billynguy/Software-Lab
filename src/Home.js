933b48ad2c52e30e1c10f6f7a910abd1268ede3f
BobbyChiu <bychiu@utexas.edu>
2022-10-25 14:12:05 -0500

    Added error checking for no network and for when the server returns a response that's not ok

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import ProjectList from "./ProjectList";
 const Home = () => {
     const [projects, setProjects] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     // COMMMENTED as we will eventually make delete request to server
     // const handleDelete = (id) => {
     //     const newProjects = projects.filter(project => project.id !== id); // leaves orig array unchanged, and returns a new array based on orig array
@@ -14,17 +15,26 @@ const Home = () => {
     useEffect(() => {                           // can't make this async
         fetch('http://localhost:8000/projects')
             .then(res => {                      // 'then' fires a function once the fetch promise has resolved, ie once we have the data back. Reponse object res is NOT the data
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resource.') // Can connect to server but response is not normal
+                }
                 return res.json()               // passes the json into a javascript object, the 'return res.json()' returns a promise b/c res.json() is aync
             })
             .then(data => {                     // we get the data now   
                 setProjects(data);
                 setIsPending(false);
+                setError(null); // gets rid of error message in (successful) subsequent requests
+            })
+            .catch(err => { // Catches any network error
+                setIsPending(false);
+                setError(err.message);
             })       
-        ;
+        
     }, []);                                     // fetch this data once when the component renders
 
     return (  
         <div className="home">
+            { error && <div>{error}</div>}
             { isPending && <div>Loading...</div>}
             {projects && <ProjectList projects={projects} title="All Projects" />}
         </div>
