e8328e08ea970a20635a7f2e3b806027633db60a
BobbyChiu <bychiu@utexas.edu>
2022-10-25 12:48:41 -0500

    Added Project delete button

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,19 @@ const Home = () => {
         { title: 'Project 1', id: 1},
         { title: 'Project 2', id: 2},
         { title: 'Project 3', id: 3},
-    ])
+    ]);
+
+    const handleDelete = (id) => {
+        const newProjects = projects.filter(project => project.id !== id); // leaves orig array unchanged, and returns a new array based on orig array
+        setProjects(newProjects);
+    }
     return (  
     <div className="home">
         <h1>Projects</h1>
         {projects.map((project) =>(
             <div className="project-preview" key={project.id}>
                 <h2>{ project.title}</h2>
+                <button onClick={() => handleDelete(project.id)}>Delete Project</button>
             </div>
         ))}
 
