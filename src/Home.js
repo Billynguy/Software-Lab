616e8e53febb4b40cb97b84a621191895a913797
BobbyChiu <bychiu@utexas.edu>
2022-11-06 18:07:19 -0600

    Added routing for adding users when creating new project

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,12 @@ import ProjectList from "./ProjectList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-    const { data: projects, isPending, error} = useFetch('http://localhost:8000/projects');
+    //const { data: projects, isPending, error} = useFetch('http://localhost:8000/projects');
+    const {data: projects} = fetch('/api/bchiu/project-list/');
 
     return (  
         <div className="home">
-            { error && <div>{error}</div>}
-            { isPending && <div>Loading...</div>}
-            {projects && <ProjectList projects={projects} title="All Projects" />}
+            {projects}
         </div>
 
     );  
