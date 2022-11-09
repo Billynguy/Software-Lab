f6eb59ff1f1ac057e3d3f3d0a83083b6c47799d1
BobbyChiu <bychiu@utexas.edu>
2022-11-08 19:43:43 -0600

    Added logout button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+
     return (  
         <nav className="navbar">
             <h1>ECE461L Website</h1>
             <div className="links">
                 <Link to="/home">Home</Link>  
                 <Link to="/create">Create Project</Link>
+                <Link to="/" onClick={() => fetch('/api/sign-out')}>Logout</Link>
                 
             </div>    
         </nav>
