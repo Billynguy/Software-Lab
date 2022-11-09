8bb4713868725d5805e93578fb4b1e147a2947b3
BobbyChiu <bychiu@utexas.edu>
2022-11-08 18:53:53 -0600

    Added backend route linking

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,34 @@ import useFetch from "./useFetch";
 
 const Home = () => {
     //const { data: projects, isPending, error} = useFetch('http://localhost:8000/projects');
-    const {data: projects} = fetch('/api/bchiu/project-list/');
+    const [projectItems, setProjectItems] = useState([]);
+    useEffect(
+        () => {
+            fetch('/api/user/session-project-list', {
+                method: 'GET'
+            }).then(res => res.json()).then(json => {
+                setProjectItems(json.data.projects);
+            });
+        }, []
+    );
+
+    const projectItemsRender = projectItems.map((project) => {
+        return (<li key={project}><a href={`/projects/${project}`}>{`Project ${project}`}</a></li>);
+    })
+
+    // {method: 'GET'}).then(res => res.json()).then(data => console.log(data))
+
+    // const projectItems = projects.map((aproject) => {
+    //     <li key={aproject.toString()}>
+    //         {aproject}
+    //     </li>
+    // }
+    // );
 
     return (  
         <div className="home">
-            {projects}
+            {/* <ul>{projectItemsRender}</ul> */}
+            {projectItemsRender}
         </div>
 
     );  
