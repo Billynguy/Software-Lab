75b8e2d94193d54a88362ab45bc86666c9ed9dd4
Billy <bhnguyen@utexas.edu>
2022-11-10 16:35:01 -0600

    Fix directories for deployment

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import atexit
 
 # Entry point
 def create_app():
-    app = Flask(__name__, static_url_path='', static_folder='../build/')
+    app = Flask(__name__, static_url_path='', static_folder='frontend/build/')
 
     @app.route('/', defaults={'path': ''})
+    @app.route('/<string:path>')
     @app.route('/<path:path>')
     def catch_all(path):
-        return send_from_directory('../build/', 'index.html')
+        return send_from_directory('frontend/build/', 'index.html')
+
+    @app.route('/projects/<string:path>')
+    @app.route('/projects/<path:path>')
+    def catch_allb(path):
+        return send_from_directory('frontend/build/', 'index.html')
 
     app.secret_key = os.environ["FLASK_SECRET_KEY"]
 
